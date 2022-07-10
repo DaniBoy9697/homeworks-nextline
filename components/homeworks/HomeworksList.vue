27edace8bbc6167d7bda578f10684791a894cdfb
DaniBoy9697 <daniel.navarro9697@gmail.com>
2022-07-10 11:15:30 -0500

    fixes homeview

--- components/homeworks/HomeworksList.vue
+++ components/homeworks/HomeworksList.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class='py-8 max-w-full bg-white text-gray-700 shadow'>
+  <div class='py-8 max-w-full bg-white text-gray-700'>
     <div
       class='
-        container
+        container max-w-full
       '
     >
       <div class='w-full md:w-12/12'>
-        <div class='flex container md:px-8'>
+        <div class='flex md:px-8 border-b pb-5'>
           <span class='text-green-500 flex-none'>
             <grid-plus-icon></grid-plus-icon>
           </span>
           <h2 class='md:px-5 flex items-center text-3xl flex-auto'>
-            <b>Categorías</b>
+            <b>Task Manager</b>
           </h2>
           <div class='flex flex-initial w-48 justify-end'>
             <NuxtLink
-              to='/administracion/catalogos/categorias/nuevo'
+              to='/tareas/nueva'
               class='text-green-600 hover:text-white border border-green-600 hover:bg-green-700 focus:ring-4
               focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
                mb-2 dark:border-green-400 dark:text-green-400 dark:hover:text-white dark:hover:bg-green-600
                 dark:focus:ring-green-700'
             >
-              Crear categoría
+              Crear Tarea
             </NuxtLink>
           </div>
+        </div>
+        <div class='flex container md:px-8'>
+          <h2 class='md:px-5 mt-8 flex items-center justify-center text-2xl flex-auto'>
+            <b>Lista de tareas</b>
+          </h2>
         </div>
         <div
           class='
@@ -44,20 +49,20 @@
     </div>
     <div class='max-w-screen-xl mx-auto text-center px-4 sm:px-6 lg:px-8'>
       <div class='overflow-hidden sm:rounded-md mt-5'>
-        <template v-if='categoriesList.length === 0'>
+        <template v-if='tasksList.length === 0'>
           <p>
             Cargando...
           </p>
         </template>
         <ul v-else>
           <li
-            v-for='(category, index) in categoriesList'
+            v-for='(task, index) in tasksList'
             :key='index'
           >
             <div
               class='
                   block
-                  hover:bg-gray-50
+                  hover:bg-gray-100
                   focus:outline-none focus:bg-gray-50
                   transition
                   duration-150
@@ -65,6 +70,58 @@
                 '
             >
               <div class='px-4 py-4 sm:px-6'>
+                <div class="container flex flex-col flex-wrap items-center justify-between mx-auto md:flex-row max-w-7xl">
+                  <div class="relative flex flex-col md:flex-row">
+                      <div
+                        class='text-md leading-5 font-medium text-black truncate'
+                      >
+                        {{ task.title }}
+                      </div>
+                  </div>
+                  <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+                    <NuxtLink
+                      class='ml-2 flex-shrink-0 flex'
+                      :to='`/taeras/${task.id}/borrar`'>
+                        <span
+                          class='
+                            px-5
+                            py-2
+                            inline-flex
+                            text-xs
+                            leading-5
+                            font-semibold
+                            rounded-full
+                            bg-red-100
+                            text-red-800
+                          '
+                        >
+                          Eliminar Tarea
+                        </span>
+                    </NuxtLink>
+                    <NuxtLink
+                      class='ml-2 flex-shrink-0 flex'
+                      :to='`/tareas/${task.id}/editar`'>
+                        <span
+                          class='
+                            px-5
+                            py-2
+                            inline-flex
+                            text-xs
+                            leading-5
+                            font-semibold
+                            rounded-full
+                            bg-green-100
+                            text-green-800
+                          '
+                        >
+                          Editar Tarea
+                        </span>
+                    </NuxtLink>
+                  </div>
+                </div>
+              </div>
+
+              <!--div class='px-4 py-4 sm:px-6'>
                 <div class='flex items-center justify-between'>
                   <div
                     class='text-sm leading-5 font-medium text-blue-600 truncate'
@@ -91,7 +148,7 @@
                       </span>
                   </NuxtLink>
                 </div>
-              </div>
+              </div-->
             </div>
           </li>
         </ul>
@@ -111,8 +168,7 @@ export default {
   data() {
     return {
       token:'e864a0c9eda63181d7d65bc73e61e3dc6b74ef9b82f7049f1fc7d9fc8f29706025bd271d1ee1822b15d654a84e1a0997b973a46f923cc9977b3fcbb064179ecd',
-      categoriesList: [],
-      dataReturned: {}
+      tasksList: [],
     }
   },
   mounted() {
@@ -122,16 +178,12 @@ export default {
     async getCategoriesData() {
     //  this.$vs.loading({ scale: 0.65, type: 'radius' })
       try {
-        this.dataReturned = await this.$store.dispatch('tasks/getTaskList', this.token)
-        this.categoriesList = this.dataReturned
+        this.tasksList = await this.$store.dispatch('tasks/getTaskList', this.token)
     //    this.$vs.loading.close()
       } catch (err) {
     //    this.$vs.loading.close()
       }
     }
-   // getCategoriesData(){
-    // this.categoriesList = [{id:1, name:"categoria1"},{id:2, name:"categoria2"}]
-   // },
   }
 }
 </script>
